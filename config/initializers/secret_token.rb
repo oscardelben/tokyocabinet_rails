0dbd241308816f29eaac6b74464e693059327c2a
Oscar Del Ben <oscar@ithere.local.lan>
2010-11-01 18:28:49 +0100

    Remove active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TokyocabinetRails::Application.config.secret_token = '48ed82a4a71a467e7bf8025f98045aaad35c3849a4d5ff19e8634a70cc4bdd2dd3588211df5d4676abd38391fbfeddebddcdab5b65399e46848eeb40fe9fc3d3'
